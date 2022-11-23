6fc82ee80213f6594a6a273b374b262f690414b0
Devarapalli Subbarao <subbu870@gmail.com>
2022-11-23 20:30:13 +0530

    Exception changed by    DSR

--- GlobalExceptionHandler1.java
+++ GlobalExceptionHandler1.java
@@ -2,6 +2,8 @@ package com.sb.exception;
 
 import java.util.Date;
 
+//DSRAO DEVELOPER CHANGE THE CODE*******
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,7 +22,27 @@ public class GlobalExceptionHandler1 {
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	package com.profile.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileControllr {
+    @Value("${message}")
+    String message;
+    
+
+ 
+    @GetMapping("/welcome")
+    public String getMessage()
+    {
+        return "Hi "+message;
     }
 
+
+}
 
 }
